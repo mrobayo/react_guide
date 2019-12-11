66950336be9ae9d9444e2ad1a46eec1947a2f09d
mrobayo <mrobayo@hotmail.com>
2019-12-10 19:12:02 -0500

    til 335 redux on burgerBuilder

--- burgerapp/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burgerapp/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -9,7 +9,8 @@ import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
 import Spinner from '../../components/UI/Spinner/Spinner';
 import axios from '../../axios-orders';
 import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
-import * as actionTypes from '../../store/actions';
+//import * as actionTypes from '../../store/actions';
+import * as burgerBuilderActions from '../../store/actions/index';
 
 const INGREDIENT_PRICES = {
     salad: 0.5,
@@ -27,13 +28,14 @@ class BurgerBuilder extends Component {
         //ingredients: null,
         totalPrice: 4,
         purchasable: false,
-        purchasing: false,
-        loading: false,
-        error: false
+        purchasing: false
+        //loading: false,
+        //error: false
     }
 
     componentDidMount() {        
         console.log(this.props);
+        this.props.onInitIngredients();
         /* axios.get('https://awesome-2fba0.firebaseio.com/ingredients.json')
         .then(response => {
             this.setState({ingredients: response.data});
@@ -121,7 +123,7 @@ class BurgerBuilder extends Component {
         }
 
         let orderSummary = null;        
-        let burger = this.state.error ? <p>Ingredients can't be loaded!</p>: <Spinner />;
+        let burger = this.props.error ? <p>Ingredients can't be loaded!</p>: <Spinner />;
         
         if (this.props.ings) {
             burger = (<Aux>
@@ -142,9 +144,9 @@ class BurgerBuilder extends Component {
                 purchaseCancelled={this.purchaseCancelHandler}
                 purchaseContinued={this.purchaseContinueHandler} />;
         }
-        if (this.state.loading) {
-            orderSummary = <Spinner />;
-        }
+        //if (this.state.loading) {
+        //    orderSummary = <Spinner />;
+        //}
 
         // {salad: true, meat: false, ...}
         return (
@@ -161,13 +163,15 @@ class BurgerBuilder extends Component {
 const mapStateToProps = state => {
     return {
         ings: state.ingredients,
-        price: state.totalPrice
+        price: state.totalPrice,
+        error: state.error
     }
 }
 const mapDispatchToProps = dispatch => {
     return {
-        onIngredientAdded: (ingName) => dispatch({type: actionTypes.ADD_INGREDIENT, ingredientName: ingName}),
-        onIngredientRemoved: (ingName) => dispatch({type: actionTypes.REMOVE_INGREDIENT, ingredientName: ingName})
+        onIngredientAdded: (ingName) => dispatch(burgerBuilderActions.addIngredient(ingName)),
+        onIngredientRemoved: (ingName) => dispatch(burgerBuilderActions.removeIngredient(ingName)),
+        onInitIngredients: () => dispatch(burgerBuilderActions.initIngredients())
     }
 }
 
---
+++ burgerapp/src/store/actions/burgerBuilder.js
@@ -0,0 +1,44 @@
+import * as actionTypes from './actionTypes';
+import axios from '../../axios-orders';
+
+export const addIngredient = (name) => {
+    return {
+      ingredientName: name,
+      type: actionTypes.ADD_INGREDIENT  
+    };
+};
+
+export const removeIngredient = (name) => {
+    return {
+      ingredientName: name,
+      type: actionTypes.REMOVE_INGREDIENT  
+    };
+};
+
+export const setIngredients = (ingredients) => {
+    return {
+        type: actionTypes.SET_INGREDIENTS,
+        ingredients: ingredients
+    }
+};
+
+export const fetchIngredientsFailed = () => {
+    return {
+        type: actionTypes.FETCH_INGREDIENTS_FAILED
+    };
+};
+
+export const initIngredients = () => {
+    return dispatch => {
+        axios.get('https://awesome-2fba0.firebaseio.com/ingredients.json')
+        .then(response => {
+            //this.setState({ingredients: response.data});
+            dispatch(setIngredients(response.data));
+        })
+        .catch(error => {
+            //this.setState({error: true});
+            dispatch(fetchIngredientsFailed());
+        });
+    };
+};
+
