9f260d411b66a9a4a7516ac16107898584206e0f
Mario Robayo <mario.robayo@bairesdev.com>
2020-01-01 16:14:01 -0500

    Chapter 366.

--- burgerapp/src/store/actions/auth.js
+++ burgerapp/src/store/actions/auth.js
@@ -27,6 +27,9 @@ export const authFail = (error) => {
 };
 
 export const logout = () => {
+    localStorage.removeItem('token');
+    localStorage.removeItem('expirationDate');
+    localStorage.removeItem('userId');
     return {
         type: actionTypes.AUTH_LOGOUT
     }
@@ -53,6 +56,11 @@ export const auth = (email, password, isSignup) => {
         axios.post(isSignup ? SIGNUP_URL : SIGNIN_URL, authData)
             .then(response => {
                 console.log('response.data', response.data);
+                const expirationDate = new Date( new Date().getTime() + response.data.expiresIn * 1000);
+
+                localStorage.setItem('token', response.data.idToken);
+                localStorage.setItem('expirationDate', expirationDate);
+                localStorage.setItem('userId', response.data.localId);
                 dispatch(authSuccess(response.data.idToken, response.data.localId));
                 dispatch(checkAuthTimeout(response.data.expiresIn));
             })
@@ -69,4 +77,23 @@ export const setAuthRedirectPath = path => {
       type: actionTypes.SET_AUTH_REDIRECT_PATH,
       path: path
   }
+};
+
+export const authCheckState = () => {
+  return dispatch => {
+    const token = localStorage.getItem('token');
+    if (!token) {
+        dispatch(logout());
+    } else {
+        const expirationDate = new Date(localStorage.getItem('expirationDate'));
+        const userId = localStorage.getItem('userId');
+        if (expirationDate <= new Date()) {
+            dispatch(logout());
+        }else {
+            dispatch(authSuccess(token, userId));
+            dispatch(checkAuthTimeout((expirationDate.getTime() - new Date().getTime())/ 1000));
+        }
+
+    }
+  } ;
 };
