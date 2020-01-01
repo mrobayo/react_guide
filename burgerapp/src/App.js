eeccb7482c04e7d47fc54af65bcf8e02c9e14135
Mario Robayo <mario.robayo@bairesdev.com>
2020-01-01 16:31:49 -0500

    Lesson 370.

--- burgerapp/src/App.js
+++ burgerapp/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 import Layout from './hoc/Layout/Layout';
@@ -26,16 +26,27 @@ class App extends Component {
   }*/
   render () {
     //{this.state.show ? <BurgerBuilder /> : null}
+    let routes = (
+        <Switch>
+          <Route path="/auth" component={Auth} />
+          <Route path="/" exact component={BurgerBuilder} />
+          <Redirect to="/"/>
+        </Switch>
+    );
+    if (this.props.isAuthenticated) {
+      routes = (
+          <Switch>
+            <Route path="/checkout" component={Checkout} />
+            <Route path="/Orders" component={Orders} />
+            <Route path="/logout" component={Logout} />
+            <Route path="/" exact component={BurgerBuilder} />
+            <Redirect to="/"/>
+          </Switch>);
+    }
     return (
       <div>
         <Layout>      
-          <Switch>
-            <Route path="/checkout" component={Checkout} /> 
-            <Route path="/Orders" component={Orders} />
-            <Route path="/auth" component={Auth} />
-            <Route path="/logout" component={Logout} />
-            <Route path="/" exact component={BurgerBuilder} /> 
-          </Switch>
+          {routes}
            {/* <BurgerBuilder />
             <Checkout />*/}
         </Layout>
@@ -44,6 +55,12 @@ class App extends Component {
   }
 }
 
+const mapStateToProps = state => {
+  return {
+    isAuthenticated: state.auth.token !== null
+  }
+};
+
 const mapDispatchToProps = dispatch => {
   return {
     onTryAutoSignup: () => dispatch(actions.authCheckState())
@@ -51,4 +68,4 @@ const mapDispatchToProps = dispatch => {
 };
 
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
